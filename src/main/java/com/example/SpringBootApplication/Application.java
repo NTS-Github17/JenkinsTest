1eb2a5d6b75071e0cd88412bb807fb44e962ad14
tiensy05 <nguyentiensy2k17@gmail.com>
2024-07-18 10:19:18 +0700

    update

--- src/main/java/com/example/SpringBootApplication/Application.java
+++ src/main/java/com/example/SpringBootApplication/Application.java
@@ -1,15 +1,23 @@
 package com.example.SpringBootApplication;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements org.springframework.boot.CommandLineRunner {
 
+	public static final Logger logger = org.slf4j.LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
+		logger.info("==================== Application started ====================");
 		SpringApplication.run(Application.class, args);
 
 		System.out.println("====================Hello World!====================");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("==================== Application running ====================");
 	}
 
 }
