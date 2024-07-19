c29cc5db92511a04205dd677cd510edb9665d3ae
tiensy05 <nguyentiensy2k17@gmail.com>
2024-07-19 16:13:51 +0700

    test sonarqube

--- src/main/java/com/example/SpringBootApplication/Application.java
+++ src/main/java/com/example/SpringBootApplication/Application.java
@@ -19,4 +19,9 @@ public class Application implements org.springframework.boot.CommandLineRunner {
 		System.out.println("============================================================ Hello World! ============================================================");
 	}
 
+	public void testSonarQube() {
+		String unusedVariable = "This variable is not used";
+		System.out.println("This is a test for SonarQube");
+	}
+
 }
