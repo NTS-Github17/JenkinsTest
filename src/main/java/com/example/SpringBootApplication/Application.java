a9766cb81b451f1cfcf0889518d96fec66f08afa
tiensy05 <nguyentiensy2k17@gmail.com>
2024-07-25 15:02:53 +0700

    test deploy from remote server 3

--- src/main/java/com/example/SpringBootApplication/Application.java
+++ src/main/java/com/example/SpringBootApplication/Application.java
@@ -30,17 +30,55 @@ public class Application implements org.springframework.boot.CommandLineRunner {
 		System.out.println("This is a test for SonarQube");
 	}
 
-	public void testSonarQubeError() throws IOException {
-		FileInputStream file = null;
-		try {
-			file = new FileInputStream("non_existent_file.txt");
-		} catch (FileNotFoundException e) {
-			// Do nothing
-			logger.error("Error: File not found.", e);
-		} finally {
-			if (file != null) {
-				file.close();
-			}
-		}
-	}
+//	public void testSonarQubeError() throws IOException {
+//		FileInputStream file = null;
+//		try {
+//			file = new FileInputStream("non_existent_file.txt");
+//		} catch (FileNotFoundException e) {
+//			// Do nothing
+//			logger.error("Error: File not found.", e);
+//		} finally {
+//			if (file != null) {
+//				file.close();
+//			}
+//		}
+//	}
+
+//	public void testSonarQubeWarning() {
+//		int a = 0;
+//		int b = 0;
+//		int c = a + b;
+//		System.out.println("The sum of a and b is: " + c);
+//	}
+
+	//	public void divisionByZero() {
+//		try {
+//			int number = 10;
+//			int result = number / 0;
+//			System.out.println("Result: " + result);
+//		} catch (ArithmeticException e) {
+//			// Do nothing
+//			System.out.println("Error: Division by zero is not allowed.");
+//		}
+//	}
+
+//	public void stringToInteger() {
+//		try {
+//			String str = "abc";
+//			int number = Integer.parseInt(str);
+//			System.out.println("Converted number: " + number);
+//		} catch (NumberFormatException e) {
+//			System.out.println("Error: Cannot convert string to integer.");
+//		}
+//	}
+//
+//	public void nullPointerDereference() {
+//		String str = null;
+//		try {
+//			System.out.println("Length of the string is: " + str.length());
+//		} catch (NullPointerException e) {
+//			// Do nothing
+//			// System.out.println("Error: Attempted to dereference a null pointer.");
+//		}
+//	}
 }
