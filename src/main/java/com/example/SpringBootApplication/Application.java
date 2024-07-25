ef5350d9efdbf3f9b36e52a1b8d575e2b536ca63
tiensy05 <nguyentiensy2k17@gmail.com>
2024-07-25 14:53:49 +0700

    test deploy from remote server 1

--- src/main/java/com/example/SpringBootApplication/Application.java
+++ src/main/java/com/example/SpringBootApplication/Application.java
@@ -36,6 +36,7 @@ public class Application implements org.springframework.boot.CommandLineRunner {
 			file = new FileInputStream("non_existent_file.txt");
 		} catch (FileNotFoundException e) {
 			// Do nothing
+			System.out.println("Error: File not found.");
 		} finally {
 			if (file != null) {
 				file.close();
@@ -49,4 +50,35 @@ public class Application implements org.springframework.boot.CommandLineRunner {
 		int c = a + b;
 		System.out.println("The sum of a and b is: " + c);
 	}
+
+//	public void divisionByZero() {
+//		try {
+//			int number = 10;
+//			int result = number / 0;
+//			System.out.println("Result: " + result);
+//		} catch (ArithmeticException e) {
+//			// Do nothing
+//			System.out.println("Error: Division by zero is not allowed.");
+//		}
+//	}
+
+//	public void stringToInteger() {
+//		try {
+//			String str = "abc";
+//			int number = Integer.parseInt(str);
+//			System.out.println("Converted number: " + number);
+//		} catch (NumberFormatException e) {
+//			System.out.println("Error: Cannot convert string to integer.");
+//		}
+//	}
+//
+//	public void nullPointerDereference() {
+//		String str = null;
+//		try {
+//			System.out.println("Length of the string is: " + str.length());
+//		} catch (NullPointerException e) {
+//			// Do nothing
+//			// System.out.println("Error: Attempted to dereference a null pointer.");
+//		}
+//	}
 }
