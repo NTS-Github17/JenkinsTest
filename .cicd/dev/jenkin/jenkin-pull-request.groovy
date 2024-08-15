8307c9e2fa36bd3491dd758726483a239319c940
DESKTOP-LN3MUE7\RESDII-101 <nguyentiensy2k17@gmail.com>
2024-08-15 15:44:46 +0700

    add pull request groovy file to dev branch

---
+++ .cicd/dev/jenkin/jenkin-pull-request.groovy
@@ -0,0 +1,119 @@
+boolean sonarQubeAnalysisDone = false
+
+pipeline {
+    agent any
+    tools {
+        maven 'maven'
+    }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+    }
+
+    environment {
+        REPO_CREDENTIALS = credentials('pat_github')
+        SONARQUBE_AUTH_TOKEN = credentials('sonarqube-auth-token')
+        GITHUB_TOKEN = credentials('github-token')
+    }
+
+    stages {
+        stage('Checkout SCM') {
+            steps {
+                script {
+                    checkPullRequestStatus()
+                    checkout([
+                            $class           : 'GitSCM',
+                            branches         : [[name: "${ghprbActualCommit}"]],
+                            extensions       : [],
+                            userRemoteConfigs: [[
+                                                        credentialsId: 'pat_github',
+                                                        name         : 'origin',
+                                                        url          : 'https://github.com/NTS-Github17/JenkinsTest.git',
+                                                        refspec      : "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
+                                                ]]
+                    ])
+                }
+            }
+        }
+
+        stage('SonarQube Analysis & Quality Gate') {
+            steps {
+                script {
+                    checkPullRequestStatus()
+                    withSonarQubeEnv(installationName: 'sonar') {
+                        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                    }
+                    echo 'Checking Quality Gate...'
+                    sonarQubeAnalysisDone = true
+                    timeout(time: 10, unit: 'MINUTES') {
+                        def qg = waitForQualityGate()
+
+                        echo "Quality Gate: ${qg}"
+
+                        if (qg.status != 'OK') {
+                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                        }
+                    }
+
+                    def qgInfo = getQualityGateInfo()
+                }
+            }
+        }
+
+        stage('Build') {
+            steps {
+                checkPullRequestStatus()
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+
+        stage('Which Java?') {
+            steps {
+                sh 'java --version'
+            }
+        }
+
+    }
+
+    post {
+        always {
+            if (sonarQubeAnalysisDone) {
+                echo 'Sending email notification...'
+                script {
+                    def qg = waitForQualityGate()
+                    emailext(
+                            subject: "Jenkins Pipeline Failure: ${env.JOB_NAME} [${env.BUILD_NUMBER}]",
+                            // Lấy kết quả từ SonarQube và gửi qua email khi sonarqube fail hoặc không pass quality gate
+                            body: """<p>Project: ${env.JOB_NAME}</p>
+                            <p>Build Number: ${env.BUILD_NUMBER}</p>
+                            <p>Quality gate status: ${qg.status}</p>
+                            <p>Cause: ${currentBuild.description}</p>
+                            <p>More details at: <a href="http://10.79.60.7:9000/dashboard?id=com.resdii.vars3d:vars-3d-ms-core">SonarQube</a></p>
+                            """,
+                            recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'DevelopersRecipientProvider']]
+                    )
+                }
+            } else {
+                echo "SonarQube analysis was not performed, skipping quality gate check."
+            }
+        }
+    }
+}
+
+def checkPullRequestStatus() {
+    def response = httpRequest(
+            url: "https://api.github.com/repos/NTS-Github17/JenkinsTest/pulls/${ghprbPullId}",
+            customHeaders: [[name: 'Authorization', value: "token ${GITHUB_TOKEN}"]],
+            httpMode: 'GET',
+            validResponseCodes: '200'
+    )
+
+    echo "GITHUB_TOKEN: ${GITHUB_TOKEN}"
+    echo "Response: ${response.content}"
+
+    def jsonResponse = readJSON text: response.content
+    def prState = jsonResponse.state
+    if (prState == 'closed') {
+        currentBuild.description = "PR ${ghprbPullId} is closed. Aborting build."
+        error "Pull request is closed. Aborting pipeline."
+    }
+}
