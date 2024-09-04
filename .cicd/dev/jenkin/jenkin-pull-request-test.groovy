4bed9685e5f40dbd0b7877cd0ffd9c27e6b21ccb
DESKTOP-LN3MUE7\RESDII-101 <nguyentiensy2k17@gmail.com>
2024-09-04 18:22:11 +0700

    test pull request 123456789

--- .cicd/dev/jenkin/jenkin-pull-request-test.groovy
+++ .cicd/dev/jenkin/jenkin-pull-request-test.groovy
@@ -30,7 +30,7 @@ pipeline {
             steps {
                 script {
 //                    checkPullRequestStatus()
-                    echo 'Checking out PR branch...123456'
+                    echo 'Checking out PR branch...123456789'
                     checkout scmGit(
                             branches: [[name: 'origin/pr/*/merge']],
                             extensions: [cleanBeforeCheckout(deleteUntrackedNestedRepositories: true)],
