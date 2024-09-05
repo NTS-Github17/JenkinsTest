a2c790af9d3800d952cf82289ba82632715c669b
DESKTOP-LN3MUE7\RESDII-101 <nguyentiensy2k17@gmail.com>
2024-09-05 11:37:31 +0700

    test pull request

--- .cicd/dev/jenkin/jenkin-pull-request-test.groovy
+++ .cicd/dev/jenkin/jenkin-pull-request-test.groovy
@@ -14,9 +14,6 @@ pipeline {
         SONARQUBE_AUTH_TOKEN = credentials('sonarqube-auth-token')
         GITHUB_TOKEN = credentials('github-token')
     }
-
-
-    
     stages {
         stage('Prepare Workspace') {
             steps {
