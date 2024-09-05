d3657f72e8acbc328f08421bc212fe34ee227635
DESKTOP-LN3MUE7\RESDII-101 <nguyentiensy2k17@gmail.com>
2024-09-05 17:26:21 +0700

    update

--- .cicd/dev/jenkin/jenkin-commit.groovy
+++ .cicd/dev/jenkin/jenkin-commit.groovy
@@ -17,6 +17,7 @@ pipeline {
         REGISTRY_CREDS = credentials('dockerhub-resdii')
         CONTAINER_NAME = "ci-cd-test"
     }
+    
     stages {
         stage('Prepare Workspace') {
             steps {
